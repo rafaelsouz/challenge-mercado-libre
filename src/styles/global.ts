3ff36d958d8b1879aa141f499e4632fc509e56a2
C1322116 Rafael Souza Santos <c1322116@interno.bb.com.br>
2022-02-24 17:23:26 -0300

    Feat: Adicionando swap no carregamento das fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 300;
+    font-display: swap;
     src: local(''),
         url('/fonts/roboto-v29-latin-300.woff2') format('woff2'), 
   }
@@ -19,6 +20,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 400;
+    font-display: swap;
     src: local(''),
         url('/fonts/roboto-v29-latin-regular.woff2') format('woff2'),
   }
@@ -27,6 +29,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 500;
+    font-display: swap;
     src: local(''),
         url('/fonts/roboto-v29-latin-500.woff2') format('woff2'), 
   }
